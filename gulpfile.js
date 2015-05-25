d8d63d886e46140b16bba32f5698ae8ce00fb015
baohe.oyt <baohe.oyt@alibaba-inc.com>
2015-05-25 20:03:07 +0800

    include

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,64 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var sass = require('gulp-sass');
+//var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var contentIncluder = require('gulp-content-includer');
+var fs = require('fs');
 
-//合并js
-gulp.task('libs',function(){
-    gulp.src('./www/js/**/*.js')
-        .pipe(concat("libs.js"))    //www/js下所有的js文件 合并到libs.js
-        .pipe(gulp.dest("./www/lib/"))  //合并后文件放入目标文件夹
-        .pipe(uglify())                   //混淆文件
-        .pipe(rename("libs.min.js"))    //重命名
-        .pipe(gulp.dest('./output/www/lib/'))       //将混淆后文件放入目标文件夹
+var prefix = './wd';
+
+gulp.task('layout', function() {
+    (function (path, cb){
+        fs.readdir(path, function(err, files){
+            //err 为错误 , files 文件名列表包含文件夹与文件
+            if(err){console.log(err); return;}
+
+            files.forEach(function(file){
+                fs.stat(path + '/' + file, function(err, stat){
+                    if(err){console.log(err); return;}
+                    if(stat.isDirectory() && file.indexOf('m_') != -1){// 如果是m_名称前缀的文件夹
+                        var path2 =  path + file;
+
+                        //读取m_目录下的文件
+                        fs.readdir(path2, function(err, files){
+                            if(err){console.log(err); return;}
+
+                            files.forEach(function(file){
+                                fs.stat(path2 + '/' + file, function(err, stat){
+                                    if(err){console.log(err); return;}
+
+                                    var modFile = path2 + '/' + file;
+                                    var layoutFileName = path2.split('/m_')[1] + '.html';
+                                    cb(layoutFileName, modFile);
+                                });
+                            });
+                        });
+                    }
+                });
+            });
+
+        });
+    }) (prefix+ '/mods/', function(layoutFileName, modFile) {
+        console.log(layoutFileName + '==' + modFile);
+        gulp.src(modFile)
+            .pipe(contentIncluder({
+                includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g
+            }))
+            .pipe(rename(layoutFileName))
+            .pipe(gulp.dest(prefix+ '/layout/'));
+    });
 });
 
-//合并css  用scss注意所有css文件后缀为scss
-gulp.task('sass', function(done) {
-    gulp.src('./mods/example/def.scss')    //定义css统一入口app.scss文件  里面导入其他css文件（导入方式见图3图4）。
-        .pipe(sass())                       //合并
-        .pipe(gulp.dest('./mods/example/'))   //合并后放入目标文件夹
-        .pipe(minifyCss({                   //混淆
-            keepSpecialComments: 0
+gulp.task('default', function() {
+    console.log(prefix + '/mods/m_test/def.html');
+
+    gulp.src(prefix + '/mods/m_test/def.html')
+        .pipe(contentIncluder({
+            includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g
         }))
-        .pipe(rename({ extname: '.min.css' }))  //混淆后加后缀
-        .pipe(gulp.dest('./mods/example/lib/'))  //混淆文件放入目标文件夹
-        .on('end', done);
+        .pipe(rename('index2.html'))
+        .pipe(gulp.dest(prefix+ '/layout/'));
+    console.log(123123);
 });
